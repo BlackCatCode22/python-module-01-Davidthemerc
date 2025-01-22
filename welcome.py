a817823da76c0258f873fabc3144fe4d4cbb867f
Davidthemerc <dmerc90@gmail.com>
2025-01-22 10:21:30 -0800

    Updated welcome.py to use the appropriate time-based greeting

--- welcome.py
+++ welcome.py
@@ -7,6 +7,15 @@ userName = input("What is your name? ")
 # Get the time
 time = datetime.now()
 hour = time.hour
+#print (hour)
+
+# Set greeting
+if hour < 12:
+    greeting = "morning"
+elif hour >= 17:
+    greeting = "evening"
+else:
+    greeting = "afternoon"
 
 # Print Greeting
-print("Hello " + userName + ".")
+print("Hello " + userName + ". Good " + greeting + "!" )
