2d5d8c4715bb0b93d4b5893ea6ab96dd69ef9db2
Davidthemerc <dmerc90@gmail.com>
2025-01-22 10:52:53 -0800

    Updated gross_pay.py to handle overtime (1.5x) pay

--- gross_pay.py
+++ gross_pay.py
@@ -5,7 +5,21 @@ hourlyRate = float(input("Hourly pay rate? "))
 # Calculate gross pay
 grossPay = weeklyHours * hourlyRate
 grossPay = round(grossPay, 2)
+
+# If user worked more than 40 hours in the week, calculate overtime pay
+if weeklyHours > 40:
+    grossPay = hourlyRate * 40
+    overHours = weeklyHours - 40
+    overPay = overHours * (hourlyRate * 1.5)
+    grossPay += overPay
+    overPay = str(overPay)
+
+# Stringify gross pay
 grossPay = str(grossPay)
 
+
 # Print gross pay
-print("Your gross pay is: $" + grossPay)
+if weeklyHours > 40:
+    print("Your gross pay is: $" + grossPay + ". This includes $" + overPay + " of overtime pay.")
+else:
+    print("Your gross pay is: $" + grossPay)
